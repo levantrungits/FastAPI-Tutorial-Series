3ba74ef75e88df9edadd869378652c9cc00d03e3
Trung LeVan <levantrungits@gmail.com>
2023-10-08 21:19:46 +0700

    Request Body

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from enum import Enum
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# deprecated=True
+# ("/", description="This is our first route.", deprecated=True)
 @app.get("/", description="This is our first route.")
 async def base_get_router():
     return {"message": "[GET] Hello, world!"}
@@ -29,6 +30,7 @@ async def get_user(user_id: str):
 async def get_current_user():
     return {"message": "this is current user"}
 
+
 class FoodEnum(str, Enum):
     fruits = "fruits"
     vegetables = "vegetables"
@@ -46,6 +48,7 @@ async def get_food(food_name: FoodEnum):
         }
 
     return {"food_name": food_name, "message": "i like chocolate milk"}
+
 
 fake_items_db = [
     {"item_name": "Foo"},
@@ -83,3 +86,24 @@ async def get_user_item(user_id: str, item_id: str, q: str | None = None, short:
             }
         )
         
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+      
+@app.post("/items")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
